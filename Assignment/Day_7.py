a7a442c0465af74c93ce7e81b488686efcfbb9d6
2020mt93043 <2020mt93043@wilp.bits-pilani.ac.in>
2023-03-07 09:17:43 +0530

    Day_8 assignment

--- Assignment/Day_7.py
+++ Assignment/Day_7.py
@@ -7,12 +7,12 @@ driver.implicitly_wait(20)
 driver.maximize_window()
 
 # Task - 1
-# driver.get("https://github.com/login")
-# time.sleep(3)
-# driver.find_element(By.XPATH, "//input[@id='login_field']").send_keys("hello")
-# driver.find_element(By.XPATH, "//input[@id='password']").send_keys("89hello")
-# driver.find_element(By.XPATH, "//input[@name='commit']").click()
-# time.sleep(3)
+driver.get("https://github.com/login")
+time.sleep(3)
+driver.find_element(By.XPATH, "//input[@id='login_field']").send_keys("hello")
+driver.find_element(By.XPATH, "//input[@id='password']").send_keys("89hello")
+driver.find_element(By.XPATH, "//input[@name='commit']").click()
+time.sleep(3)
 
 # Task - 2
 driver.get("https://www.salesforce.com/in/form/signup/freetrial-sales/")
---
+++ Assignment/Day_8.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(20)
+driver.maximize_window()
+
+driver.get("https://www.oracle.com/in/database")
+time.sleep(2)
+driver.find_element(By.XPATH, "//button[@data-lbl='sign-in-account']").click()
+time.sleep(2)
+driver.find_element(By.XPATH, "//a[normalize-space()='Sign-In']").click()
+time.sleep(3)
+actual_title = driver.title
+print(actual_title)
+actual_url = driver.current_url
+print(actual_url)
+actual_header = driver.header
+print(actual_header)
