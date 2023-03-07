b27134d72a26d98a161d73b45260e769d0b246e0
2020mt93043 <2020mt93043@wilp.bits-pilani.ac.in>
2023-03-07 09:39:52 +0530

    Added Task 2 for Day 8

--- Assignment/Day_8.py
+++ Assignment/Day_8.py
@@ -6,6 +6,7 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(20)
 driver.maximize_window()
 
+# Task - 1
 driver.get("https://www.oracle.com/in/database")
 time.sleep(2)
 driver.find_element(By.XPATH, "//button[@data-lbl='sign-in-account']").click()
@@ -13,12 +14,27 @@ time.sleep(2)
 driver.find_element(By.XPATH, "//a[normalize-space()='Sign-In']").click()
 time.sleep(3)
 actual_title = driver.title
-print(actual_title)
+print("Title", actual_title)
 actual_url = driver.current_url
-print(actual_url)
-actual_header = driver.header
-print(actual_header)
+print("URL", actual_url)
 
 driver.find_element(By.XPATH, "//input[@name='ssousername']").send_keys("john")
 driver.find_element(By.XPATH, "//input[contains(@title,'Please enter a password')]").send_keys("john123")
 driver.find_element(By.XPATH, "//input[contains(@title,'Please click here to sign in')]").click()
+time.sleep(3)
+
+
+# Task - 2
+driver.get("https://www.medibuddy.in/")
+time.sleep(3)
+driver.find_element(By.XPATH, "//a[normalize-space()='Login']").click()
+driver.find_element(By.XPATH, "//div[contains(text(),'I have a Corporate Account')]").click()
+driver.find_element(By.XPATH, "//div[contains(text(),'Login using Username & Password')]").click()
+driver.find_element(By.XPATH, "//input[@placeholder='Enter Username']").send_keys("john")
+driver.find_element(By.XPATH, "//button[normalize-space()='Proceed']").click()
+driver.find_element(By.XPATH, "//input[@placeholder='Enter Password']").send_keys("john123")
+driver.find_element(By.XPATH, "//input[@type='checkbox']").click()
+driver.find_element(By.XPATH, "//button[normalize-space()='Log in']").click()
+actual_error_text = driver.find_element(By.XPATH, "//div[@class='alert alert-danger errorTxt']").text
+print(actual_error_text)
+time.sleep(5)
